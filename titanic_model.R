c61fbcee3253f4157d03ec643fed5ec6d61e9d80
Ivan Oliveri <ipoliveri@gmail.com>
2016-09-03 10:49:43 -0300

    Added Log to Normalize the Fare

--- titanic_model.R
+++ titanic_model.R
@@ -2,11 +2,9 @@ library(pROC)
 
 library(plyr)
 
-library(randomForest)
+kFilePath <- "C:/Users/Ivan/Documents/GitHub/titanic-kaggle-competition/"
 
-#kFilePath <- "C:/Users/Ivan/Documents/Kaggle/Titanic/"
-
-kFilePath <- "C:/Users/user/Documents/GitHub/titanic-kaggle-competition/"
+#kFilePath <- "C:/Users/user/Documents/GitHub/titanic-kaggle-competition/"
 
 kTrainFile <- "train.csv"
 
@@ -53,16 +51,21 @@ for(oneDataSet in lst.datasets){
   
   #Replace NA Values with Column Mean for Fare
   
-  oneDataSet[is.na(oneDataSet[,"Fare"]), "Fare"] <- mean(oneDataSet[,"Fare"], na.rm = TRUE)
+  oneDataSet[is.na(oneDataSet[,"Fare"]), "Fare"] <- median(oneDataSet[,"Fare"], na.rm = TRUE)
+  
+  oneDataSet$Fare <- ifelse(oneDataSet$Fare==0,0,log(oneDataSet$Fare))
   
   #Creation of Ticket Category Vector
   
   vec.ticket <- as.character(oneDataSet$Ticket)
   
-  vec.ticketCategory <- as.factor(unlist(lapply(vec.ticket,
-                                                function(x) ifelse(!is.na(as.numeric(substr(x,1,1))),
-                                                                   ifelse(as.numeric(substr(x,1,1))>3 || as.numeric(substr(x,1,1)) ==0,
-                                                                          "Other Number",substr(x,1,1)),substr(x,1,1)))))
+  vec.ticketCategory <- unlist(lapply(vec.ticket, function(x) ifelse(is.na(as.numeric(unlist(strsplit(x," "))[1]))==T,
+                                                  unlist(strsplit(x," "))[1],"No City Defined")))
+  
+  vec.ticketCategory <-gsub("\\/","",toupper(gsub("\\.","",vec.ticketCategory)))
+  
+  vec.ticketCategory <- ifelse(vec.ticketCategory=="NO CITY DEFINED","NO CITY DEFINED",
+                               substr(vec.ticketCategory,1,1))
   
   #Create Dummy Variables for PClass, Embarked, Cabin and Ticket Category
   
@@ -72,15 +75,25 @@ for(oneDataSet in lst.datasets){
   
   vec.partialNames <- unlist(lapply(oneDataSet$Name, function(x) trimws(as.character(unlist(strsplit(as.character(x),split=","))[2]))))
   
-  vec.titles <- unlist(lapply(vec.partialNames, function(x) as.character(unlist(strsplit(as.character(x),split=" "))[1])))
+  vec.titles <- gsub("\\.","",unlist(lapply(vec.partialNames, function(x) as.character(unlist(strsplit(as.character(x),split=" "))[1]))))
   
-  vec.titlesFiltered <- ifelse(!vec.titles %in% c("Master.","Miss.","Mr.","Mrs."),"Other",vec.titles)
+  vec.titles[vec.titles %in% c("Capt", "Col", "Dr", "Major", "Rev", "Sir")] <- "Noble"
+  
+  vec.titles[vec.titles %in% c("Dona", "Ms", "the","Lady")] <- "Mrs"
+  
+  vec.titles[vec.titles %in% c("Mme", "Mlle")] <- "Miss"
+  
+  vec.titles[vec.titles %in% c("Jonkheer","Don")] <- "Mr"
   
   #Create Dummy Variable for Cabin
   
   vec.cabin <- ifelse(!substring(oneDataSet$Cabin,1,1) %in% c("A","B","C","D","E","F"),"Other",substring(oneDataSet$Cabin,1,1))
   
-  #Include Only Q and S. Otherwise it would fail since the other levels aren't included in the testset
+  #Create Dummy Variable for Fare
+  
+  vec.fareIsZero <- as.factor(as.numeric(oneDataSet$Fare==0))
+  
+  #Include Only Q and S. Otherwise it would fail since the other levels aren"t included in the testset
   
   mat.Embarked <- model.matrix( ~ oneDataSet$Embarked - 1)[,c("oneDataSet$EmbarkedQ","oneDataSet$EmbarkedS")]
   
@@ -88,9 +101,10 @@ for(oneDataSet in lst.datasets){
   
   mat.ticketCategory <- model.matrix( ~ vec.ticketCategory - 1)
   
-  mat.titles <- model.matrix( ~ vec.titlesFiltered - 1)
+  mat.titles <- model.matrix( ~ vec.titles - 1)
   
-  df.dummyDataset <- data.frame(mat.Pclass, mat.Embarked, mat.cabin, mat.ticketCategory, mat.titles)
+  df.dummyDataset <- data.frame(mat.Pclass, mat.Embarked, mat.cabin, 
+                                mat.ticketCategory, mat.titles, vec.fareIsZero)
   
   df.dummyDataset[,1:length(df.dummyDataset)] <- lapply(df.dummyDataset[,1:length(df.dummyDataset)],
                                                         function(x) as.factor(x))
@@ -123,7 +137,7 @@ for(oneDataSet in lst.datasets){
     
     #Create Dummy Variable for Mother Indicator
     
-    vec.motherIndicator <- factor(as.numeric((oneDataSet$Age>=18 & oneDataSet$vec.titlesFilteredMiss.==1 & 
+    vec.motherIndicator <- factor(as.numeric((oneDataSet$Age>=18 & oneDataSet$vec.titlesMiss==1 & 
                                                 oneDataSet$Sex=="female" & oneDataSet$Parch>0)))
     
     #Create Dummy Variable for Child Indicator
@@ -157,7 +171,7 @@ for(oneDataSet in lst.datasets){
   
   #Create Dummy Variable for Mother Indicator
   
-  vec.motherIndicator <- factor(as.numeric((oneDataSet$Age>=18 & oneDataSet$vec.titlesFilteredMiss.==1 & 
+  vec.motherIndicator <- factor(as.numeric((oneDataSet$Age>=18 & oneDataSet$vec.titlesMiss==1 & 
                                               oneDataSet$Sex=="female" & oneDataSet$Parch>0)))
   
   #Create Dummy Variable for Child Indicator
